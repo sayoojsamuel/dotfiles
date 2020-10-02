50db06fadd1de852827a0e208350b859bee331c8
sayoojsamuel <sayoojsamuelgreat@gmail.com>
2020-10-02 12:48:33 +0530

    Restructure the doom configs

---
+++ doom_old/doom/.doom.d/config.el
@@ -0,0 +1,13 @@
+;;; .doom.d/config.el -*- lexical-binding: t; -*-
+
+
+;; Place your private configuration here
+
+;; Package definitions
+
+;; emacs M-arrows shortcut
+;;; (after! org
+;;;   (map! :map org-mode-map
+;;;         :n "M-j" #'org-metadown
+;;;         :n "M-k" #'org-metaup)
+;;; )
